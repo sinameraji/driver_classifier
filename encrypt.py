c64ff18fd9f7d26aab6f83b53713465cdbd22bd4
Sina Meraji <sina.meraji@gmail.com>
2018-12-16 15:07:57 +0800

    im ded

---
+++ encrypt.py
@@ -0,0 +1,6 @@
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+
+key = get_random_bytes(16)
+cipher = AES.new(key, AES.MODE_EAX)
+ciphertext, tag = cipher.encrypt_and_digest('sina, name, etc')
